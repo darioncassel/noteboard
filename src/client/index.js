e85c1c1a91fc6250d0fb83b632774af6769ca4b8
raddar4 <darioncassel@d-172-26-4-58.bootp.Virginia.EDU>
2015-02-22 19:36:36 -0500

    Initial upload
    *Still undocumented*

---
+++ src/client/index.js
@@ -0,0 +1,172 @@
+//@DarionCassel
+Router.configure({
+	layoutTemplate: 'ApplicationLayout'
+});
+
+Router.onBeforeAction(function () {
+	if (!Meteor.user()) {
+		this.render('login');
+	} else {
+		this.next();
+	}
+});
+
+Router.route('/', function(){
+	this.render('Main');
+	this.render('footer', {to:'footer'});
+});
+
+NotesData = new Mongo.Collection("notes");
+
+if(Meteor.isClient){
+
+	/*new code*/
+	Template.login.rendered = function() {
+		$("a.initPop").fancybox({
+			helpers: {
+				overlay: null
+			},
+			'autoSize': true,
+			'closeBtn': false,
+			'loop': false,
+			'nextSpeed': 2000,
+			'nextEffect': 'none'
+		});
+		//if ($.cookie('modal_shown') == null) {
+			//$.cookie('modal_shown', 'yes', { expires: 10000, path: '/' });
+			//$('#init1').click();
+		//}
+		$('#closeWelcome').click(function() {
+			$.fancybox.close();
+		});
+	};
+	/*end new*/
+
+	Template.login.events({
+		'click button[name=login]': function() {
+			var user=randomString(10);
+			var pass=randomString(10);
+			var color="#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+			if (Modernizr.geolocation) {
+				navigator.geolocation.getCurrentPosition(setLoc);
+				$('#loginBtn').html('Loading...');
+			} else {
+				$('#loginBtn').html('Error.');
+				alert("Error: GPS enabled?");
+			}
+			function setLoc(position){
+				Accounts.createUser({username:user, password:pass, profile: {color: color, loc: [position.coords.longitude, position.coords.latitude]}});
+				Meteor.loginWithPassword(user, pass);
+				Meteor.subscribe("NotesData_near");
+				Meteor.subscribe("Users_self");
+				Meteor.subscribe("Users_near");
+			}
+		}
+	});
+
+	Template.footer.events({
+		'click #add' : function () {
+			if(!$('#noteInput').val().match(/^\s*$/)){
+				var note = {
+						text: $('#noteInput').val(),
+						age: 0,
+						color: Meteor.user().profile.color,
+						timestamp: moment().format('X'),
+						loc: Meteor.user().profile.loc
+				};
+				Meteor.call('addNote', note);
+			}
+			$('#noteInput').val('');
+			$('#noteInput').focus();
+		},
+		'keypress input' : function (event) {
+			if(event.which===13){
+				if(!$('#noteInput').val().match(/^\s*$/)){
+					var note = {
+						text: $('#noteInput').val(),
+						age: 0,
+						color: Meteor.user().profile.color,
+						timestamp: moment().format('X'),
+						loc: Meteor.user().profile.loc
+					};
+					Meteor.call('addNote', note);
+				}
+				$('#noteInput').val('');
+			}
+		}
+	});
+
+	Template.footer.helpers({
+		users: function() {
+			return Meteor.users.find({_id: {$ne: Meteor.userId()}}).fetch();
+		}
+	});
+
+	Template.main.helpers({
+		notes: function() {
+			return NotesData.find().fetch();
+		}
+	});
+
+	Template.note.rendered = function() {
+		$('#notesdata').scrollTop($('#notesdata').prop("scrollHeight"));
+	}
+
+	Handlebars.registerHelper('linkify',  function(text){
+		var linkedText = Autolinker.link(text, {stripPrefix: false});
+		var url = /<a\b[^>]*>(.*?)<\/a>/;
+		var x = linkedText.match(url);
+		console.log(x);
+		return new Handlebars.SafeString(linkedText);
+	});
+
+	function randomString(num) {
+		var text = "";
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for(var i=0;i<num;i++ ){
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
+
+	Tracker.autorun(function() {
+		setInterval(function() {
+			if(Meteor.user()){
+				navigator.geolocation.getCurrentPosition(setLoc);
+				function setLoc(position){
+					Meteor.users.update({_id: Meteor.userId}, {$set: {loc: [position.coords.longitude, position.coords.latitude]}});
+				}
+			}
+		}, 300000);
+	});
+
+	Meteor.startup(function() {
+		Session.setDefault('document-title', 'noteboard');
+		Deps.autorun(function() {
+			document.title = Session.get('document-title');
+		});
+	});
+
+	var focused = true;
+	var counter = 0;
+	ifvisible.on("blur", function(){
+		focused = false;
+	});
+
+	ifvisible.on("focus", function(){
+		Session.set('document-title', 'noteboard');
+		focused = true;
+		counter = 0;
+	});
+
+	NotesData.find().observe({
+		_suppress_initial: true,
+		added: function() {
+			if(!focused){
+				counter++;
+				Session.set('document-title', '('+counter+') noteboard');
+			}
+		}
+	});
+
+}
---
+++ src/server/server.js
@@ -0,0 +1,101 @@
+//@DarionCassel
+Fiber = Npm.require('fibers');
+NotesData = new Mongo.Collection("notes");
+
+if(Meteor.isServer) {
+
+  Accounts.onLogin(function() {
+    Meteor.publish("Users_self", function() {
+      self = this;
+      if(this.userId){
+        return Meteor.users.find({_id: this.userId},
+        {fields: {'profile.color': 1, 'profile.loc': 1}});
+      }else {
+        console.log('FAILURE-1');
+      }
+    });
+    Meteor.publish("Users_near", function() {
+      self = this;
+      if(this.userId){
+        var loc = Meteor.users.findOne(this.userId).profile.loc;
+        return Meteor.users.find({
+          'profile.loc': {
+            $geoWithin: {
+              $center: [loc, 0.0002699784] //30 meters
+            }
+          }
+        }, {fields:{'profile.color':1}});
+      }else {
+        //return Meteor.users.find();
+        console.log('FAILURE-2');
+      }
+    });
+    Meteor.publish("NotesData_near", function() {
+      self = this;
+      if(this.userId){
+        var loc = Meteor.users.findOne(this.userId).profile.loc;
+        return NotesData.find({
+          loc: {
+            $geoWithin: {
+              $center: [loc, 0.0002699784] //30 meters
+            }
+          }
+        }, {sort: {timestamp: 1}});
+      }else {
+        //return NotesData.find();
+        console.log('FAILURE-3');
+      }
+    });
+  });
+
+  UserStatus.events.on("connectionLogout", function(fields) {
+    Meteor.users.remove({_id: fields.userId});
+  });
+
+  UserStatus.events.on("connectionIdle", function(fields) {
+    Meteor.users.remove({_id: fields.userId});
+  });
+
+  Tracker.autorun(function() {
+    setInterval(function() {
+      Fiber(function(){
+        var tempArr = NotesData.find().fetch();
+        if(tempArr!=[]){
+          for(i=0; i<tempArr.length; i++){
+            if(tempArr[i].age!=undefined && tempArr[i].age>59){
+              NotesData.remove(tempArr[i]._id);
+            }
+          }
+        }
+      }).run();
+    }, 120000);
+    setInterval(function() {
+      Fiber(function(){
+        var tempArr = NotesData.find().fetch();
+        for(i=0;i<tempArr.length;i++){
+          var time = Math.floor((moment().format('X') - tempArr[i].timestamp)/60);
+          if(time<=0){time=0;}
+          NotesData.update(tempArr[i]._id, {$set: {age: time}});
+        }
+      }).run();
+    }, 10000);
+  });
+
+  Meteor.methods({
+    addNote: function(text) {
+      if(!Meteor.userId()){
+        throw new Meteor.Error("not-authorized");
+      }else {
+        NotesData.insert(text);
+      }
+    },
+    removeAllNotes: function() {
+      NotesData.remove({});
+    }
+  });
+
+  Meteor.startup(function() {
+    Meteor.users._ensureIndex({'profile.loc': "2d" });
+    NotesData._ensureIndex({loc: "2d"});
+  });
+}
